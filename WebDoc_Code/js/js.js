44db405cedc82dfef00f303bbd3cbf796a676779
AmandaJakse <73820132+amandajakse@users.noreply.github.com>
2021-03-03 15:22:46 +0100

    added missing sections

--- WebDoc_Code/js/js.js
+++ WebDoc_Code/js/js.js
@@ -3,3 +3,33 @@
  * purpose: Whatever goes here ...
  **/
 console.log('JavaScript from js/js.js: up and running!');
+
+//----------------------------------------------------
+let myBth = document.getElementById ('playButton')
+myBth.addEventListener ("click", function playVideo(){
+  if (myVideo.paused) {
+    myVideo.play();
+    myVideo.controls = false;
+      }
+  else {
+    myVideo.pause();
+    myVideo.controls = true;
+    }
+})
+
+shutup.addEventListener('click',
+  function(){
+    myVideo.muted = true;
+  });
+
+//-----------------------------------
+
+
+
+
+/**
+let myStatus = [
+    "We deliver"
+    "We don't deliver anymore"
+]
+**/
